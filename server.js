8f2f8ab9b3adb2f99f826f37a774930273a2f9c3
Ben Cason <bencason9@gmail.com>
2019-05-30 18:16:21 -0400

    server fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 //set up express
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //set up data parsing
 app.use(express.urlencoded({ extended: true }));
