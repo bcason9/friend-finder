ae94f4d59c5ca7ea438265e0115c48fb76a6a289
Ben Cason <bencason9@gmail.com>
2019-06-01 09:02:40 -0400

    updated code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,22 @@
-let friendsData = []
+let friendsData = [
+    {
+        "name": "Casey",
+        "photo": "link to photo",
+        "scores": [
+          "5",
+          "5",
+          "3",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "3",
+          "5"
+        ]
+
+    }
+]
 
 
 module.exports = friendsData
